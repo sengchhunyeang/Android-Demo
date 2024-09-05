3ae85e8fe9085f632f27736e7185d1304ab5ffd8
Seng Chhunyeang <chhunyeangseng@gmail.com>
2024-09-05 13:29:22 +0700

    psuh test

--- app/src/main/java/com/example/material_design/MainActivity.kt
+++ app/src/main/java/com/example/material_design/MainActivity.kt
@@ -1,48 +1,28 @@
 package com.example.material_design
 
-import NestedNavigation
+import com.example.material_design.NestedNavigation.NestedNavigation
 import android.os.Bundle
-import android.widget.CheckBox
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
-import com.example.material_design.Grid.GridLayOut
-import com.example.material_design.Grid.LazyVerticalGrid.Flag
-import com.example.material_design.Grid.LazyVerticalGrid.LazyVerticalGrid
-import com.example.material_design.Grid.LazyVerticalGrid.MyGrid
-import com.example.material_design.Layout.Avatar
-import com.example.material_design.MaterialDesign.AllButton
-import com.example.material_design.MaterialDesign.DropDown
-import com.example.material_design.MaterialDesign.MainScreen
-import com.example.material_design.MaterialDesign.MyAlertDialog
-import com.example.material_design.MaterialDesign.TestCheck
-import com.example.material_design.Scaffold.SmallTopAppBarExample
 import com.example.material_design.ui.theme.Material_DesignTheme
-import java.util.Properties
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/material_design/NestedNavigation/Graph.kt
+++ app/src/main/java/com/example/material_design/NestedNavigation/Graph.kt
@@ -1,21 +1,17 @@
+package com.example.material_design.NestedNavigation
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -23,7 +19,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import java.security.AccessController
 
 @Composable
 fun MainNavGraph(navController: NavHostController) {
